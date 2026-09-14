6952c5e1e785d81aa6ff755c277bcf860977c3af
Carlos <pepitogrillo148@hotmail.com>
2026-09-14 02:23:39 +0200

    feat: We have created the Application class in order to simplify our application and the main fail

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
 import sys
 from PySide6.QtWidgets import QApplication
-from src.controllers.home_controller import HomeController
-from src.models.home_model import HomeModel
-from src.views.home_view import HomeView
-from src.views.main_window import MainWindow
-
+from src.core.application import Application
 
 def main():
     """Start the application."""
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    home_view = HomeView()
-    home_model = HomeModel()
-    home_controller = HomeController(view=home_view, model=home_model,)
-    window.add_view(home_view)
-    window.show_view(home_view)
-    window.show()
-    return app.exec()
+    qt_application = QApplication(sys.argv)
+    application = Application(qt_application)
+    application.start()
+    return qt_application.exec()
 
 if __name__ == "__main__":
     sys.exit(main())
